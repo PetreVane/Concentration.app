a80efb77fdf66032c496add951d0a615c6db16b7
Petre Vane <petre.vane@gmail.com>
2018-07-09 19:32:03 +0200

    changing layout

--- TableView Demo/TableView Demo/ViewController.swift
+++ TableView Demo/TableView Demo/ViewController.swift
@@ -20,9 +20,11 @@ class ViewController: UIViewController, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         // printing the number of rows
-        cell.textLabel?.text = "\(indexPath.row)"
+        cell.textLabel?.text = "Testing \(indexPath.row)"
+        cell.textLabel?.textColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
         return cell
     }
+    
     
 
     override func viewDidLoad() {
