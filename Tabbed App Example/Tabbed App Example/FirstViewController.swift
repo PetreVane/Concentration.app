ee8b9f64f64b2a4eb4a8a00fef459c7364310bfc
Petre Vane <petre.vane@gmail.com>
2018-07-10 19:22:30 +0200

    Example of app with multiple tabs

---
+++ Tabbed App Example/Tabbed App Example/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Tabbed App Example
+//
+//  Created by Petre Vane on 10/07/2018.
+//  Copyright © 2018 Petre Vane. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
