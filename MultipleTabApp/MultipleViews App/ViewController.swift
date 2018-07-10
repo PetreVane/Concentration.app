d1b23280fac9c53f256bbec1a76b1fc37e172db4
Petre Vane <petre.vane@gmail.com>
2018-07-10 19:24:24 +0200

    Example of app with multiple tabs

---
+++ MultipleTabApp/MultipleViews App/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MultipleViews App
+//
+//  Created by Petre Vane on 10/07/2018.
+//  Copyright © 2018 Petre Vane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "First View Controller"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
