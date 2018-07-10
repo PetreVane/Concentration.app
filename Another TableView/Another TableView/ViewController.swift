58bd758fcf3c428b315d50a2d5b97445a9d72154
Petre Vane <petre.vane@gmail.com>
2018-07-10 14:04:36 +0200

    Adding method for reusing table cells

--- Another TableView/Another TableView/ViewController.swift
+++ Another TableView/Another TableView/ViewController.swift
@@ -8,31 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = "here is cell number \(indexPath.row)"
+       
+        let cell = tableView.dequeueReusableCell(withIdentifier: "normalCell", for: indexPath)
+        cell.textLabel?.text = "Here is your cell number \(indexPath.row) "
+//        switch indexPath.section {
+//        case 0, 1, 2:
+//            cell.textLabel?.text = "here is section \(indexPath.section) and cell \(indexPath.row)"
+//        default:
+//            cell.textLabel?.text = "You should not get this"
+//        }
         return cell
     }
     
 
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 5
-    }
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return 3
+//    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        switch section {
-        case 0:
-            return 0
-        case 1:
-            return 1
-        case 2:
-            return 2
-        default:
-            return 10
-        }
+        
+        return 300
+//        switch section {
+//        case 0:
+//            return 25
+//        case 1:
+//            return 25
+//        case 2:
+//            return 25
+//        default:
+//            return 0
+//        }
     }
     
     override func viewDidLoad() {
