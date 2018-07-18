3bdb3daeb462d4ef1fc750bac10c75db7d00da0d
Petre Vane <petre.vane@gmail.com>
2018-07-18 17:19:41 +0200

    Setting background and buttons

---
+++ DiceGame/DiceGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DiceGame
+//
+//  Created by Petre Vane on 18/07/2018.
+//  Copyright © 2018 Petre Vane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
