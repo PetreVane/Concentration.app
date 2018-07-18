99772cb6865ef2cf60415cd61a69677e543bfb65
Petre Vane <petre.vane@gmail.com>
2018-07-18 19:23:24 +0200

    adding randomisation

--- DiceGame/DiceGame/ViewController.swift
+++ DiceGame/DiceGame/ViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var randomDiceIndex = 0
+    var randomDiceIndex2 = 0
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
+    
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        randomDiceIndex = Int(arc4random_uniform(7))
+        randomDiceIndex2 = Int(arc4random_uniform(7))
+        print (randomDiceIndex)
+        //diceImageView1.image
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
