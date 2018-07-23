5f58fac6d02bd85fba43fbdc3fa726a6a673b071
Petre Vane <petre.vane@gmail.com>
2018-07-23 21:08:01 +0200

    bridged sounds with button's tag

--- SingingApp/Xylophone/ VIewController.swift
+++ SingingApp/Xylophone/ VIewController.swift
@@ -7,23 +7,54 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController{
+
+class ViewController: UIViewController, AVAudioPlayerDelegate {
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
-
+// creating an object
+    var audioPLayer: AVAudioPlayer!
+    
+// getting all the sounds within an array
+    var listOfSounds = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    
+// creating a placeholder for sender.tag number, which will point to an index of the listOfSounds
+    var selectedSound: String = ""
+    
+    
+    
     @IBAction func notePressed(_ sender: UIButton) {
         
+// pointing to an index of the listOfSounds, based on each tag of the sender
+        selectedSound = listOfSounds[sender.tag - 1]
         
+        playSound()
+            }
+    
+    func playSound() {
+        
+// creating a url to the local soundfile
+        let soundUrl = Bundle.main.url(forResource: selectedSound, withExtension: "wav")
+        
+// attempting to play the sound
+        do {
+            audioPLayer = try AVAudioPlayer.init(contentsOf: soundUrl!)
+            
+        } catch {
+            print(error)
+        }
+        
+        audioPLayer.play()
+    }
+    
         
     }
     
-  
+   
 
-}
+
 
