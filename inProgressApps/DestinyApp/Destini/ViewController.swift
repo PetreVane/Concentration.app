1d82711596b49935973940d2741a0b9090eeba26
Petre Vane <petre.vane@gmail.com>
2018-08-03 18:49:33 +0200

    Revert "added reset button"
    This reverts commit 96543163e83bcf393a441f7178bc4a1fceb59d51.

--- inProgressApps/DestinyApp/Destini/ViewController.swift
+++ inProgressApps/DestinyApp/Destini/ViewController.swift
@@ -35,7 +35,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var bottomButton: UIButton!      // Has TAG = 2
     @IBOutlet weak var storyTextView: UILabel!
     
-    @IBOutlet weak var resetButton: UIButton!
     // TODO Step 5: Initialise instance variables here
     var storyIndex = 1
     
@@ -43,8 +42,10 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        resetButton.isHidden = true
+        storyTextView.text = story1
+        topButton.setTitle(answer1a, for: .normal)
+        bottomButton.setTitle(answer1b, for: .normal)
+        storyIndex = 1
         
         // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
         
@@ -72,21 +73,16 @@ class ViewController: UIViewController {
                 storyTextView.text = story4
                 topButton.isHidden = true
                 bottomButton.isHidden = true
-                storyIndex = 4
     
         } else if storyIndex == 3 && sender.tag == 1 {
                 storyTextView.text = story6
                 topButton.isHidden = true
                 bottomButton.isHidden = true
-                storyIndex = 6
             
         } else if storyIndex == 3 && sender.tag == 2 {
                 storyTextView.text = story5
                 topButton.isHidden = true
                 bottomButton.isHidden = true
-                storyIndex = 5
-            
-       
     
         // TODO Step 4: Write an IF-Statement to update the views
                 
@@ -95,30 +91,8 @@ class ViewController: UIViewController {
     
     }
     
-        switch storyIndex {
-        case 4, 5, 6:
-            resetButton.isHidden = false
-        default:
-            print("End of line")
-        }
-        
 }
 
-    @IBAction func restartButton(_ sender: UIButton) {
-        
-            resetGame()
-    }
-    
-    func resetGame() {
-        storyTextView.text = story1
-        topButton.isHidden = false
-        bottomButton.isHidden = false
-        topButton.setTitle(answer1a, for: .normal)
-        bottomButton.setTitle(answer1b, for: .normal)
-        storyIndex = 1
-        resetButton.isHidden = true
-    }
-    
-    
+
 }
 
