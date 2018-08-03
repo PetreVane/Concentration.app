e81626c63339906191665a755a5893ac8c07b0a2
Petre Vane <petre.vane@gmail.com>
2018-08-03 20:12:26 +0200

    first commit

---
+++ inProgressApps/SingingApp/Xylophone/ VIewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 27/01/2016.
+// ReCreated by Petre Vane on July 2018
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+  
+
+}
+
