3c18757c33e067b9351a360532863c2bdcda757c
Petre Vane <petre.vane@gmail.com>
2018-08-05 14:42:36 +0200

    first commit

--- AbandonedApps/StackViewMessingAround/StackViewMessingAround/ViewController.swift
+++ AbandonedApps/StackViewMessingAround/StackViewMessingAround/ViewController.swift
@@ -9,7 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var firstInts = 0
+    var intsOnScreen = 0
+    var nextInt = 0
+    var pressedIndex = 0 // counts the number of time a button is pressed
+    
+    
+    @IBOutlet weak var screen: UILabel!
+    
+    
+    @IBAction func intButtonPressed(_ sender: UIButton) {
+        
+            screen.text?.append(String(sender.tag))
+            firstInts = Int(screen.text!)!
+            print(sender.tag)
+        
+        switch sender.tag {
+        case 10:
+            screen.text?.append("")
+            print(firstInts)
+            print(screen)
+        default:
+            print("something else")
+        }
+        
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
