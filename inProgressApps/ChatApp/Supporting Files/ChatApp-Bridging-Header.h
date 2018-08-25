3ed64fd6b5e4c7881ae7a00613d5268c55c4edd0
Petre Vane <petre.vane@gmail.com>
2018-08-25 17:06:08 +0200

    imported libraries and added bridging file

---
+++ inProgressApps/ChatApp/Supporting Files/ChatApp-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  ChatApp-Bridging-Header.h
+//  ChatApp
+//
+//  Created by Petre Vane on 25/08/2018.
+//  Copyright © 2018 Petre Vane. All rights reserved.
+//
+
+#ifndef ChatApp_Bridging_Header_h
+#define ChatApp_Bridging_Header_h
+
+
+#endif /* ChatApp_Bridging_Header_h */
+
+#import "SWRevealViewController.h"
